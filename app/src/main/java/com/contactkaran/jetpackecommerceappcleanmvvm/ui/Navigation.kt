0cfe65fcc20837a37a78e333c949610d8351d47b
Kardy <ksm@mailu.com>
2023-06-22 21:30:50 -0400

    App Name change

---
+++ app/src/main/java/com/contactkaran/jetpackecommerceappcleanmvvm/ui/Navigation.kt
@@ -0,0 +1,19 @@
+package com.contactkaran.jetpackecommerceappcleanmvvm.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.contactkaran.jetpackecommerceappcleanmvvm.ui.home.HomeScreen
+
+enum class Routes{
+    Home,
+    Detail
+}
+
+@Composable
+fun ShoppingNavigation(
+    navHostController: NavHostController
+) {
+
+}
